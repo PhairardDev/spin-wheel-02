d5fa1dd6d8f42f8cc3e79ab1eb073f49e93aaae7
Dev360 <tok.bet.360@gmail.com>
2022-03-18 13:53:30 +0700

    Thursday Update

--- api/api_coupon_check.php
+++ api/api_coupon_check.php
@@ -9,7 +9,20 @@ if($_SERVER['REQUEST_METHOD']=='GET'){
     echo json_encode($db->query('SELECT * FROM coupons ORDER BY id DESC'));
 
 } else if ($_SERVER['REQUEST_METHOD']=='POST') {
-    echo 'This is Post!!';
+    
+    $coupons = $_POST['str_redeem'];
+    /*$sql = "SELECT COUNT(couponsCode) FROM coupons WHERE couponsCode = :redeem";
+    $result = $db->prepare($sql);
+    $result->execute(array(':redeem'=>$_POST['str_redeem']));*/
+
+    if(isset($coupons)) {
+        echo $coupons;
+    }
+    else {
+        echo '300';
+        return http_response_code(300);
+    }
+
 } else {
     http_response_code(405);
 }
