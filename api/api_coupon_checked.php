6d394f09467abe8e5b7ae8d152061c4684fd1f7b
Witcher DEV <tok.bet.360@gmail.com>
2022-03-22 00:08:08 +0700

    update wheel and API

--- api/api_coupon_checked.php
+++ api/api_coupon_checked.php
@@ -1,12 +1,21 @@
 <?php 
 if(isset($_POST['str_redeem'])){ 
     
-    $redeem=$_POST['str_redeem'];
-    echo $_POST['str_redeem'];
+    $redeem = $_POST['str_redeem'];
     $code_ck = "80DYPAO9Y9";
-    return $code_ck;
+
+    if($redeem == $code_ck){
+        echo $code_ck;
+        return $code_ck;
+    }
+    else {
+        echo "400";
+        return http_response_code(400);
+    }
+    
+    
 } 
 else {
     echo "300";
-    return http_response_code(300);;
+    return http_response_code(300);
 }
