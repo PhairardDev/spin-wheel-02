376d59740c75eac9c4a9a4894e202cebbcca87d2
Dev360 <tok.bet.360@gmail.com>
2022-04-07 11:29:36 +0700

    Update api_coupon_checked.php

--- api/api_coupon_checked.php
+++ api/api_coupon_checked.php
@@ -56,15 +56,3 @@ if($_SERVER['REQUEST_METHOD']=='POST' && $_POST['str_redeem'] != "" ){
 else {
     http_response_code(405);
 }
-
-/*function getRandomWeightedElement(array $weightedValues) {
-    $rand = mt_rand(1, (int) array_sum($weightedValues));
-
-    foreach ($weightedValues as $key => $value) {
-      $rand -= $value;
-      if ($rand <= 0) {
-        return $key;
-      }
-    }
-  }
-*/
